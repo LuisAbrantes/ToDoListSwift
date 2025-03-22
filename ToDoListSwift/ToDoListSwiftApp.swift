bd34eb299946e47e1426fc802bd4675f7633a8de
Luis Henrique dos Santos Abrantes <luis.hsa@gmail.com>
2025-03-22 15:31:25 -0300

    Initial version

---
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftApp.swift
+//  ToDoListSwift
+//
+//  Created by Luis Abrantes on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
